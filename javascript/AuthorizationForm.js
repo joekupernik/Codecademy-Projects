28ed93dfff756c3e35788d1a09052650e9e1eee8
Joe Kupernik <75756646+joekupernik@users.noreply.github.com>
2023-01-13 10:18:35 -0800

    Create AuthorizationForm.js

---
+++ javascript/AuthorizationForm.js
@@ -0,0 +1,55 @@
+/*
+A client just called you to say that they love their new website! There’s only one problem: they don’t like how their contact page displays their personal information for all to see.
+
+They’ve asked you to hide their website’s contact page behind a password form. In this project, you’ll accomplish this by using a React component to set up a simple authorization layer.
+*/
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      password: 'swordfish',
+      authorized: true
+    };
+    this.authorize = this.authorize.bind(this);
+  }
+
+  authorize(e) {
+    const password = e.target.querySelector(
+      'input[type="password"]').value;
+    const auth = password == this.state.password;
+    this.setState({
+      authorized: auth
+    });
+  }
+
+  render() {
+    const login = (
+    <form action:='#' onSubmit={this.authorize}>
+    <input type='password' placeholder='Password'></input>
+    <input type='submit'></input>   
+    </form>)
+    const contactInfo = (<ul>
+          <li>
+            client@example.com
+          </li>
+          <li>
+            555.555.5555
+          </li>
+        </ul>);
+    
+    return (
+      <div id="authorization">
+        <h1>{this.state.authorized ? 'contact' : 'enter the password'}</h1>
+        {this.state.authorized ? contactInfo : login}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Contact />, 
+  document.getElementById('app')
+);
